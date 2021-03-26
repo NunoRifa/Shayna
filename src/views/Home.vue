d20e0cbfe9a61fe1c0ba79ad131d90a72ba84745
NunoRifa <nuno180202@gmail.com>
2021-03-26 15:29:11 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShyna />
+    <HeroShyna />
+    <BannerShyna />
+    <InstaShyna />
+    <LogoShyna />
+    <FooterShyna />
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  // import HelloWorld from "@/components/HelloWorld.vue";
+  import HeaderShyna from "@/components/HeaderShyna.vue";
+  import HeroShyna from "@/components/HeroShyna.vue";
+  import BannerShyna from "@/components/BannerShyna.vue";
+  import InstaShyna from "@/components/InstaShyna.vue";
+  import LogoShyna from "@/components/LogoShyna.vue";
+  import FooterShyna from "@/components/FooterShyna.vue";
+
+  export default {
+    name: "Home",
+    components: {
+      HeaderShyna,
+      HeroShyna,
+      BannerShyna,
+      InstaShyna,
+      LogoShyna,
+      FooterShyna,
+    },
+  };
+</script>
